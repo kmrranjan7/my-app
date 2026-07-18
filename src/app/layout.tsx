be56e014732a736d681244b419a952b620439989
Kumar Ranjan <kumar.ranjan@standardbank.co.za>
2026-07-19 01:44:58 +0530

    card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   viewportFit: "cover",
 };
 
